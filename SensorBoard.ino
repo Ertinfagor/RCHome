bca748aa4b74080f6e37e27622906d674e5d69ed
Andrii Bereznikov <abereznik@gmail.com>
2013-12-23 19:49:58 +0200

    Update Sketch
    Add Switch on/of test
    Add Basic parsing of command

--- SensorBoard.ino
+++ SensorBoard.ino
@@ -5,7 +5,8 @@
 RF24 radio(9,10);
 
 const uint64_t pipes[2] = { 0xF0F0F0F001LL, 0xF0F0F0F002LL };
- 
+const byte shortAddr = 0x02;
+int led = 13;
 void setup(void)
 {
   Serial.begin(57600);
@@ -19,6 +20,12 @@ void setup(void)
   radio.openWritingPipe(pipes[0]);
   
   radio.startListening();
+  
+  // Testing switch on/off
+   pinMode(led, OUTPUT);  
+ 
+  
+  
 }
 
 void loop(void)
@@ -36,13 +43,28 @@ void loop(void)
 	// make the transition to receiver
 	delay(20);
       }
-      runCommand(command[1]);
+      runCommand(command[1],command[2]);
     }
 
 }
-bool runCommand(byte command){Serial.println(command);}
+bool runCommand(byte command, byte parameter)
+{
+  Serial.println(command);
+  switch (command)
+  {
+    case 0:
+      break;
+    case 1:
+      switchOnLight(parameter);
+      break;
+    case 2:
+      switchOffLight(parameter);
+      break;
+  }
+
+}
 byte readTemp(void){}
 byte readHum(void){}
 byte readLight(void){}
-bool switchOnLight(void){}
-bool switchOffLight(void){}
+bool switchOnLight(byte number){digitalWrite(led, HIGH);}
+bool switchOffLight(byte number){digitalWrite(led, LOW);}
