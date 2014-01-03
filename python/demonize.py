d2b2e370def882fd6c11114a5e09049f7ee84bf7
Ertinfagor <abereznik@gmail.com>
2014-01-03 14:02:49 +0000

    Add basic commands to files for debug daemon

--- python/demonize.py
+++ python/demonize.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
 import time
-b = [0x1,0,0,0,0,0,0,0,0,0,0xff,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 # Open a file
-print b[8]
 fo = open("/Log.txt", "a+")
-for item in b:
-	fo.write("%s" % item)
-	print
+fo.write("0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0")
 fo.flush();
-time.sleep(1);
+time.sleep(2);
 print fo.readline();
-#{0x1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,i,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
