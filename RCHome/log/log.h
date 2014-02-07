602b8794d0b259bacce8490f885b49376e0f0bf5
Andrii Bereznikov <abereznik@gmail.com>
2014-02-07 23:10:47 +0200

    New branch

---
+++ RCHome/log/log.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <cstdlib>
+#include <iostream>
+
+
+class Log{
+	public:
+		Log();
+		write();
+
+
+
+
+
+
+}
