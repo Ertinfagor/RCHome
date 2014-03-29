87dc34c47ac1a01fe1a5919ad19a13b6cffd58c2
Andrii Bereznikov <abereznik@gmail.com>
2014-03-29 04:19:14 +0200

    Cleanup

--- Archive/Controller/interface/demonize.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import time
-# Open a file
-fo = open("/Log.txt", "a+")
-fo.write("0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0")
-fo.flush();
-time.sleep(2);
-print fo.readline();
