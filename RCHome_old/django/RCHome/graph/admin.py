a54b5499f3a3b77b84012a3cc31ede4566e45fc0
Ertinfagor <abereznik@gmail.com>
2014-04-06 13:23:40 +0300

    Revert "(testing)New version of controller"
    This reverts commit 5d359c9f92c702f7897ca09eb2e8a6639ea78fcf.
    
    Conflicts:
    	Controller/Makefile
    	Controller/main.cpp
    	Controller/rchome/rchome.cpp
    	Controller/rchome/rchome.h
    	RCHome/lib/newver/rchome/rchome.cpp
    	RCHome/lib/newver/rchome/rchome.h
    	RCHome/lib/newver/rchome/rchome/rchome.cpp
    	RCHome/lib/newver/rchome/rchome/rchome.h
    	RCHome_old/lib/librf24/Makefile
    	RCHome_old/lib/librf24/RF24.cpp
    	RCHome_old/lib/librf24/RF24.h
    	RCHome_old/lib/librf24/RF24_config.h
    	RCHome_old/lib/librf24/compatibility.c
    	RCHome_old/lib/librf24/compatibility.h
    	RCHome_old/lib/librf24/gpio.cpp
    	RCHome_old/lib/librf24/gpio.h
    	RCHome_old/lib/librf24/librf24.so.1.0
    	RCHome_old/lib/librf24/nRF24L01.h
    	RCHome_old/lib/librf24/readme.txt
    	RCHome_old/lib/librf24/spi.cpp
    	RCHome_old/lib/librf24/spi.h
    	RCHome_old/lib/newver/Makefile

--- RCHome_old/django/RCHome/graph/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from graph.models import SensorBoard1
-from graph.models import CommandTable
-
-admin.site.register(SensorBoard1)
-admin.site.register(CommandTable)
